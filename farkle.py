9fea7b90d1fc35f4d394d56f1a06fb3678f02ef9
Jonah Temple <jonah.d.temple@gmail.com>
2025-12-29 13:51:17 -0500

    Reworked scoreDice() to return a Dictionary of possible scores instead of a single, total score. Idea is to let the calling function decide which score to take, then remove those dice and either re-score or re-roll & re-score the remaining dice until the turn is over.
    Right now there's an issue with the dictionary not accepting multiple entries with the same score, will have to find some workaround for that...

--- farkle.py
+++ farkle.py
@@ -2,12 +2,13 @@ import random
 
 
 def scoreDice(dice):
-    #                1  2  3  4   5  6
-    dice_values = [100, 0, 0, 0, 50, 0]  # scores for individual dice
-    # dice is a List of up to 6 ints
+    #  this function will take an arbitrary list of Dice values and return a Dictionary of possible scores
+    #  dict will take the form of 'scores[points] = [list_of_dice]'
+    #  so a roll of [1, 1, 2, 5, 5, 6] will return {100: [1], 200: [1, 1], 50: [5], 100: [5, 5]}
+
     num_dice = len(dice)
-    values = [0,0,0,0,0,0]  # number of ones, twos, threes, etc
-    scores = [0,0,0,0,0,0]  # scores for ones, twos, threes, etc
+    values = [0, 0, 0, 0, 0, 0]  # number of ones, twos, threes, etc
+    scores = {}  # will store list of scores & their associated die
     for d in range(num_dice):
         val = dice[d] - 1  # since val is an index, and indices start at 0
         values[val] += 1
@@ -18,49 +19,77 @@ def scoreDice(dice):
         no_mults = False
 
     if values[0] == 0 and values[4] == 0 and no_mults is True:
-        return 0  # no score if no 1s, 5s, or 3+ multiples
+        return {0: 0}  # no score if no 1s, 5s, or 3+ multiples
 
     # look for runs, either 1,2,3,4,5, 2,3,4,5,6, or 1,2,3,4,5,6
     runs_score = 0
     if values[0] > 0 and values[1] > 0 and values[2] > 0 and values[3] > 0 and values[4] > 0:
-        runs_score = 750
+        scores[750] = [1, 2, 3, 4, 5]
     if values[5] > 0 and values[1] > 0 and values[2] > 0 and values[3] > 0 and values[4] > 0:
-        runs_score += 750
+        scores[750] = [2, 3, 4, 5, 6]
+    if len(scores) == 2:  # IE if both runs are present
+        scores[1500] = [1, 2, 3, 4, 5, 6]
 
     for i in range(6):
+        d = i + 1  # the number on the dice
         val = values[i]
-        dice_score = dice_values[i]
-        if val < 3:
-            scores[i] = dice_score * val
+        if d == 1:
+            if val >= 1:
+                scores[100] = [d]
+            if val >= 2:
+                scores[200] = [d, d]
+            if val >= 3:
+                scores[1000] = [d, d, d]
+            if val >= 4:
+                scores[2000] = [d, d, d, d]
+            if val >= 5:
+                scores[4000] = [d, d, d, d, d]
+            if val == 6:
+                scores[8000] = [d, d, d, d, d, d]
+        elif d == 5:
+            if val >= 1:
+                scores[50] = [d]
+            if val >= 2:
+                scores[100] = [d, d]
+            if val >= 3:
+                scores[d*100] = [d, d, d]
+            if val >= 4:
+                scores[d*200] = [d, d, d, d]
+            if val >= 5:
+                scores[d*400] = [d, d, d, d, d]
+            if val == 6:
+                scores[d*800] = [d, d, d, d, d, d]
         else:
-            if i == 0:
-                scores[i] = 1000
-            else:
-                scores[i] = (i+1) * 100
+            if val >= 3:
+                scores[d*100] = [d, d, d]
+            if val >= 4:
+                scores[d*200] = [d, d, d, d]
+            if val >= 5:
+                scores[d*400] = [d, d, d, d, d]
+            if val == 6:
+                scores[d*800] = [d, d, d, d, d, d]
 
-        if val == 4:
-            scores[i] *= 2
-        elif val == 5:
-            scores[i] *= 4
-        elif val == 6:
-            scores[i] *= 8
-
-    return sum(scores) + runs_score
+    return scores
 
 
 def main():
     print('Farkle!')
 
-    dice = [1, 1, 1, 1, 1, 1]
+    num_dice = int(input('Enter # of dice: '))
+    dice = []
+    for d in range(num_dice):
+        dice.append(0)
 
     while True:
         input('Press ENTER to roll die')
-        for d in range(6):
+        for d in range(num_dice):
             dice[d] = random.randrange(1, 7)
         dice.sort()
 
         print(dice)
-        print(scoreDice(dice))
+        scores = scoreDice(dice)
+
+        print(scores)
 
 
 if __name__ == '__main__':
